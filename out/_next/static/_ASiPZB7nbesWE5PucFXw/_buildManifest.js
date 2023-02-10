deab405ae4dfd9283ba6941c58479c38830a6e58
demian <demian@dev.io>
2023-02-11 03:24:53 +0900

    out

---
+++ out/_next/static/_ASiPZB7nbesWE5PucFXw/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/b006b331397fd480.css","static/chunks/pages/index-264e2e08d877443e.js"],"/_error":["static/chunks/pages/_error-2f4054212a845689.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
