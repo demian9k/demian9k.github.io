c8fbed2f0008cf9c747172871d6cef3766b28a75
demian <demian@dev.io>
2023-02-12 14:50:30 +0900

    social link

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -11,11 +11,7 @@ const siteMetadata = {
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
   email: 'demian11k@gmail.com',
-  github: 'https://github.com',
-  twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
+  github: 'https://github.com/demian9k',
   locale: 'en-US',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
