a70bb9ff1679fea744cf81b4c1079bd9048ed972
demian <demian@dev.io>
2023-02-12 15:10:44 +0900

    logo change

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -1,11 +1,13 @@
 import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
 import Logo from '@/data/logo.svg'
+import AvartarLogo from '@/data/demian_avatar_logo.png'
 import Link from './Link'
 import SectionContainer from './SectionContainer'
 import Footer from './Footer'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
+import Image from '@/components/Image'
 
 const LayoutWrapper = ({ children }) => {
   return (
@@ -16,7 +18,13 @@ const LayoutWrapper = ({ children }) => {
             <Link href="/" aria-label={siteMetadata.headerTitle}>
               <div className="flex items-center justify-between">
                 <div className="mr-3">
-                  <Logo />
+                  <Image
+                    alt={''}
+                    src={AvartarLogo}
+                    className="object-cover object-center md:h-36 lg:h-48"
+                    width={40}
+                    height={40}
+                  />
                 </div>
                 {typeof siteMetadata.headerTitle === 'string' ? (
                   <div className="hidden h-6 text-2xl font-semibold sm:block">
